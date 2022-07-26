878df4606350600a1645863ba8bb5be5bed46c6d
AleProsDev <alejandro.f.pros@gmail.com>
2022-07-26 09:39:56 -0300

    generamos escudo
    -creamos: animacion escudo
    - fase activacion escudo
    - fase desactivacion escudo
    -sonido de fase activacion
    -script escudo

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -23,6 +23,7 @@ onready var estela: Estela = $EstelaPuntoInicio/Trail2D
 onready var motor_sfx: Motor = $MotorSFX
 onready var colisionador:CollisionShape2D = $CollisionShape2D
 onready var impacto_sfx:AudioStreamPlayer = $ImpactosSFX
+onready var escudo:Escudo = $Escudo
 
 ## Metodos
 func _ready() -> void:
@@ -55,6 +56,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		or event.is_action_released("mover_atras")):
 			motor_sfx.sonido_off()
 	
+	# Control escudo
+
+	if event.is_action_pressed("escudo") and not escudo.get_esta_activado():
+		escudo.activar()
 		
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
