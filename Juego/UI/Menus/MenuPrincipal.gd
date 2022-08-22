ed657654a83603f6228a4700cbd6786fd43305d1
AleProsDev <alejandro.f.pros@gmail.com>
2022-08-22 02:57:23 -0300

    dialogic, cambios
    -cambios en el menu principal
    -agregamos dialogic al proyecto

--- Juego/UI/Menus/MenuPrincipal.gd
+++ Juego/UI/Menus/MenuPrincipal.gd
@@ -7,9 +7,11 @@ func _ready() -> void:
 	OS.set_window_fullscreen(true)
 	MusicaJuego.play_musica(MusicaJuego.get_lista_musicas().menu_principal)
 
-func _on_BotonJugar_pressed() -> void:
+
+func _on_Salir_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_Jugar_pressed()  -> void:
 	MusicaJuego.play_boton()
 	get_tree().change_scene(nivel_inicial)
-
-func _on_BotonSalir_pressed() -> void:
-	get_tree().quit()
